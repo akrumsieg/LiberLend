0b39668a8a7946ca252a1001e800532423679419
Aaron Krumsieg <a.krumsieg@gmail.com>
2021-06-29 17:23:28 -0500

    Add BookEdit and BookDetails models

---
+++ LiberLend.Models/BookModels/BookDetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiberLend.Models.BookModels
+{
+    public class BookDetails
+    {
+        public string ISBN { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Publisher { get; set; }
+
+        public string Description { get; set; }
+
+        public string Edition { get; set; }
+
+        public string Genre { get; set; }
+    }
+}
---
+++ LiberLend.Models/BookModels/BookEdit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiberLend.Models.BookModels
+{
+    public class BookEdit
+    {
+        [MinLength(10, ErrorMessage = "Please enter a 10- or 13-digit ISBN")]
+        [MaxLength(13, ErrorMessage = "Please enter a 10- or 13-digit ISBN")]
+        public string ISBN { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Author's First Name")]
+        public string AuthorFirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Author's Last Name")]
+        public string AuthorLastName { get; set; }
+
+        public string Publisher { get; set; }
+
+        public string Description { get; set; }
+
+        public string Edition { get; set; }
+
+        public string Genre { get; set; }
+    }
+}
