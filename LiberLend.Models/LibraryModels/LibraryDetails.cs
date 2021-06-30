ff45f7ef4e152ccb1a598947e35e4671dee6ad31
Aaron Krumsieg <a.krumsieg@gmail.com>
2021-06-30 12:29:03 -0500

    Add Library models

---
+++ LiberLend.Models/LibraryModels/LibraryDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiberLend.Models.LibraryModels
+{
+    public class LibraryDetails
+    {
+        public int LibraryId { get; set; }
+
+        public string ApplicationUserId { get; set; }
+
+        [Display(Name = "Library Name")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Display(Name = "Library Caretaker")]
+        public string CaretakerName { get; set; }
+    }
+}
---
+++ LiberLend.Models/LibraryModels/LibraryEdit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiberLend.Models.LibraryModels
+{
+    public class LibraryEdit
+    {
+        public int LibraryId { get; set; }
+
+        public string ApplicationUserId { get; set; }
+
+        [Display(Name = "Library Name")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
