f06f1a563cb56e8296259f78d58ed4e5e2396759
Aaron Krumsieg <a.krumsieg@gmail.com>
2021-07-01 09:17:35 -0500

    Add MembershipController basic CRUD

---
+++ LiberLend.WebMVC/Controllers/MembershipController.cs
@@ -0,0 +1,114 @@
+﻿using LiberLend.Models.MembershipModels;
+using LiberLend.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LiberLend.WebMVC.Controllers
+{
+    [Authorize]
+    public class MembershipController : Controller
+    {
+        private MembershipService CreateMembershipService()
+        {
+            var userId = User.Identity.GetUserId();
+            return new MembershipService(userId);
+        }
+
+        // GET: Membership
+        public ActionResult Index()
+        {
+            var service = CreateMembershipService();
+            return View(service.GetAllMemberships());
+        }
+
+        //GET: Details
+        public ActionResult Details(int id)
+        {
+            var service = CreateMembershipService();
+            return View(service.GetMembershipById(id));
+        }
+
+        //GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(MembershipCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            var service = CreateMembershipService();
+            if (service.CreateMembership(model))
+            {
+                TempData["SaveResult"] = "Membership was successfully created!";
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Membership could not be created.");
+            return View(model);
+        }
+
+        //Get: Edit
+        public ActionResult Edit(int id)
+        {
+            var service = CreateMembershipService();
+            var detail = service.GetMembershipById(id);
+            var model = new MembershipEdit
+            {
+                MembershipId = detail.MembershipId
+            };
+            return View(model);
+        }
+
+        //POST: Edit
+        //The library caretaker uses this function to toggle IsAuthorized
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, MembershipEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            if (model.MembershipId != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
+                return View(model);
+            }
+            var service = CreateMembershipService();
+            if (service.EditMembership(model))
+            {
+                TempData["SaveResult"] = "Update successful!";
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Update failed.");
+            return View(model);
+        }
+
+        //GET: Delete
+        public ActionResult Delete(int id)
+        {
+            var service = CreateMembershipService();
+            return View(service.GetMembershipById(id));
+        }
+
+        //POST: Delete
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePost(int id)
+        {
+            var service = CreateMembershipService();
+            if (service.DeleteMembership(id))
+            {
+                TempData["SaveResult"] = "This membership was deleted.";
+                return RedirectToAction("Index");
+            }
+            TempData["SaveResult"] = "Delete failed.";
+            return RedirectToAction("Index");
+        }
+    }
+}
